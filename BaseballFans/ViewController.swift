e864b72a80f39d44e7575e435a4c5a5e847c03da
Sun Huanji <sunhuanji1992@gmail.com>
2017-03-14 15:32:19 +0900

    Initial Commit

---
+++ BaseballFans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseballFans
+//
+//  Created by Sun Huanji on 2017/3/14.
+//  Copyright © 2017年 Sun Huanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
